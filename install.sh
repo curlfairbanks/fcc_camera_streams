1bd9233b0d893620be79b01e05a8bd5a039374b5
Jef Spaleta <jspaleta@gmail.com>
2020-01-20 21:47:28 -0900

    setting up for sidebyside

--- install.sh
+++ install.sh
@@ -7,9 +7,10 @@ fi
 
 echo "Installing camera control script to /usr/local/bin"
 cp python/camera_control /usr/local/bin
-
+cp python/sidebyside /usr/local/bin
 echo "Installing systemd services into /etc/systemd/system"
 cp systemd/SHEET@.service /etc/systemd/system
+cp systemd/SIDEBYSIDE@.service /etc/systemd/system
 cp systemd/camera_control.service /etc/systemd/system
 
 echo "reloading system daeamon"
