8e6286105508ea9903a174d073987c13f28b8f6d
Jon <akjonj@gmail.com>
2020-01-26 02:56:37 +0000

    Modified init to remove itself from rc.local

--- pi_init.sh
+++ pi_init.sh
@@ -3,6 +3,8 @@
 git clone http://github.com/curlfairbanks/fcc_camera_streams.git /home/pi/curl_cam
 chown -R pi:pi /home/pi/curl_cam
 
+sed -i '\/home\/pi\/pi_init.sh/d' /etc/rc.local
+
 /home/pi/curl_cam/pi_setup.sh
 
 /sbin/reboot
